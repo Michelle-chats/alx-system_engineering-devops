bee7bafae74c75ea752f85223723aecd7e9837f1
Michelle-chats <mtchatikobo@gmail.com>
2023-01-11 11:08:35 -0800

    submitting

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-# creates a file in the /tmp directory
-file { '/tmp/school':
-ensure  => 'file',
-mode    => '0744',
-owner   => 'www-data',
-group   => 'www-data',
-content => 'I Love Puppet',
-}
+#creating new file
+#file { '/tmpschool':
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+  }
